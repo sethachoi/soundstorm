bec22bec766888772b61abcdaced190b20926dc1
Alex Gian <alexhgian@gmail.com>
2016-01-30 00:13:42 -0800

    Something

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -6,11 +6,11 @@ angular.module('starter.controllers', [])
 })
 
 .controller('FriendsCtrl', function($scope, Friends) {
-  $scope.friends = Friends.all();
+    $scope.friends = Friends.all();
 })
 
 .controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
-  $scope.friend = Friends.get($stateParams.friendId);
+    $scope.friend = Friends.get($stateParams.friendId);
 })
 
 .controller('AccountCtrl', function($scope) {
@@ -25,13 +25,13 @@ angular.module('starter.controllers', [])
 
 
     $scope.shouldShowDelete = false;
- $scope.shouldShowReorder = false;
- $scope.listCanSwipe = false
+    $scope.shouldShowReorder = false;
+    $scope.listCanSwipe = false
 
-$scope.editToggle = function(){
-    $scope.shouldShowDelete = !$scope.shouldShowDelete;
-    $scope.shouldShowReorder = !$scope.shouldShowReorder;
-}
+    $scope.editToggle = function(){
+        $scope.shouldShowDelete = !$scope.shouldShowDelete;
+        $scope.shouldShowReorder = !$scope.shouldShowReorder;
+    }
 
     $scope.items = [{
         img: "http://placehold.it/512x512",
@@ -50,4 +50,36 @@ $scope.editToggle = function(){
         title: "Title 4",
         description: "2:50"
     }]
+})
+.controller('MenuCtrl', function($scope) {
+})
+.controller('HomeCtrl', function($scope, $interval) {
+    $scope.progressval = 0;
+    $scope.stopinterval = null;
+
+
+        startprogress();
+
+
+
+    function startprogress()
+    {
+        $scope.progressval = 0;
+
+        if ($scope.stopinterval)
+        {
+            $interval.cancel($scope.stopinterval);
+        }
+
+        $scope.stopinterval = $interval(function() {
+            $scope.progressval = $scope.progressval + 1;
+            if( $scope.progressval >= 1000 ) {
+                $interval.cancel($scope.stopinterval);
+                $state.go('second');
+                return;
+            }
+        }, 1000);
+    }
+
+    startprogress();
 });
