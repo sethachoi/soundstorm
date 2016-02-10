3005d9e18a909786abb5c4f4a3cdeabf719f831c
alex gian <alexhgian@gmail.com>
2016-02-10 02:20:48 -0800

    Config file

--- www/config.js
+++ www/config.js
@@ -1,3 +1,4 @@
 var config ={
-    "SOUNDCLOUD_CLIENT_ID": "cdf0a6cde22cb7171c0f2f8f1718dedd"
+    "SOUNDCLOUD_CLIENT_ID": "cdf0a6cde22cb7171c0f2f8f1718dedd",
+    "SOUNDCLOUD_CALLBACK_URL": "http://localhost:5000/callback.html"
 }
