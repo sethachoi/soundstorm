6945cd068ebce635244115cd625685987e599185
alex gian <alexhgian@gmail.com>
2016-02-10 02:13:03 -0800

    Config file

--- www/app/login/login.controller.js
+++ www/app/login/login.controller.js
@@ -1,6 +1,16 @@
 angular.module('soundstorm')
-.controller('LoginCtrl', function($scope, $window, $state) {
-
+.controller('LoginCtrl', function($scope, $window, $state, ENV) {
+    function testConnection(){
+        SC.get('/me')
+        .then(function(data){
+            console.log('inviteCallback() Auth success!');
+             $state.go('menu.home');
+        })
+        .catch(function(data){
+             console.log('inviteCallback() Auth failed!')
+             // TODO: Add some error thing
+        })
+    }
     $window.inviteCallback = function() {
          SC.get('/me')
          .then(function(data){
@@ -14,6 +24,8 @@ angular.module('soundstorm')
      }
 
     $scope.scLogin = function() {
-    	SC.connect();
+    	SC.connect().then(function(){
+            testConnection();
+        });
     }
 })
