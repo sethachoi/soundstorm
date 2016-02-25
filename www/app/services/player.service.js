17492a8520c45cebfc96725a14df81810a69ec08
alex gian <alexhgian@gmail.com>
2016-02-25 00:14:19 -0800

    Fave fix

--- www/app/services/player.service.js
+++ www/app/services/player.service.js
@@ -127,6 +127,6 @@ angular.module('soundstorm')
         'on': on,
         'isFaved': isFaved,
         'faveChecker': faveChecker,
-        'getTrackById': getTrackById
+        'getTrackById': getTrackById,
     }
 });
